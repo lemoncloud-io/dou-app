bec62523a0f8586098a3b2ff48bb7ec829b65bf9
louis <louis@lemoncloud.io>
2025-12-29 14:50:53 +0900

    feat: rename mobile app

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -11,7 +11,7 @@ const { assetExts, sourceExts } = defaultConfig.resolver;
  * @type {import('metro-config').MetroConfig}
  */
 const customConfig = {
-  cacheVersion: '@chatic-front/Chatic',
+  cacheVersion: '@chatic/mobile',
   transformer: {
     babelTransformerPath: require.resolve('react-native-svg-transformer'),
   },
