3ab233e41c514c71d0ff914805f0e46562429821
raine-lemon <raine@lemoncloud.io>
2026-02-13 14:22:52 +0900

    feat: implement getAppLanguage func - 앱 설정언어 불러오는 함수 추가

--- apps/mobile/src/app/common/utils/device.ts
+++ apps/mobile/src/app/common/utils/device.ts
@@ -1,11 +1,9 @@
-import uuid from 'react-native-uuid';
-
-import { STORAGE_KEYS, StorageService } from '../services';
-import { USER_AGENT_PREFIX_IOS, USER_AGENT_PREFIX_ANDROID } from '@chatic/device-utils';
+import { USER_AGENT_PREFIX_ANDROID, USER_AGENT_PREFIX_IOS } from '@chatic/device-utils';
 
 import DeviceInfo from 'react-native-device-info';
 import { Platform } from 'react-native';
 import Config from 'react-native-config';
+import { getLocales } from 'react-native-localize';
 
 /**
  * 플랫폼별 User Agent 식별자 접두사
@@ -16,28 +14,6 @@ export const APP_USER_AGENT_PREFIX = Platform.select({
     ios: USER_AGENT_PREFIX_IOS,
     android: USER_AGENT_PREFIX_ANDROID,
 });
-
-/**
- * Retrieves the unique device ID for the user.
- * Generates a random UUID if it doesn't already exist.
- * Managed and persisted via AsyncStorage.
- * @returns A promise that resolves to the device ID or null if an error occurs.
- * @author raine@lemoncloud.io
- */
-export const getDeviceId = async (): Promise<string | null> => {
-    try {
-        const storedId = await StorageService.get<string>(STORAGE_KEYS.USER.DEVICE_ID);
-
-        if (storedId) return storedId;
-
-        const newId = uuid.v4().toString();
-        await StorageService.set(STORAGE_KEYS.USER.DEVICE_ID, newId);
-
-        return newId;
-    } catch {
-        return null;
-    }
-};
 
 /**
  * UserAgent 불러오기
@@ -53,4 +29,17 @@ export const getUserAgent = async (): Promise<string> => {
     const platform = Platform.OS === 'ios' ? 'iOS' : 'Android';
 
     return `${systemUserAgent} (${APP_USER_AGENT_PREFIX}; ${appName}/${appVersion}; ${platform}; Build:${buildNumber})`;
+};
+
+/**
+ * App Language 불러오기
+ * - 디바이스에 설정된 언어 불러오기
+ * - 기본값 'en'
+ */
+export const getAppLanguage = (): string => {
+    const locales = getLocales();
+    if (locales.length === 0) {
+        return 'en';
+    }
+    return locales[0].languageCode;
 };
