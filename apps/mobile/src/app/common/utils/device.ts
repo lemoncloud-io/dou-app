f69442a70ae7ca197300ae7a72ba68609577707d
raine-lemon <raine@lemoncloud.io>
2026-02-13 13:28:42 +0900

    feat: add getUserAgent - APP_USER_AGENT_PREFIX 상수추가 - userAgent 불러오는 함수 추가

--- apps/mobile/src/app/common/utils/device.ts
+++ apps/mobile/src/app/common/utils/device.ts
@@ -1,6 +1,21 @@
 import uuid from 'react-native-uuid';
 
 import { STORAGE_KEYS, StorageService } from '../services';
+import { USER_AGENT_PREFIX_IOS, USER_AGENT_PREFIX_ANDROID } from '@chatic/device-utils';
+
+import DeviceInfo from 'react-native-device-info';
+import { Platform } from 'react-native';
+import Config from 'react-native-config';
+
+/**
+ * 플랫폼별 User Agent 식별자 접두사
+ * - iOS: DOU_IOS
+ * - Android: DOU_ANDROID
+ */
+export const APP_USER_AGENT_PREFIX = Platform.select({
+    ios: USER_AGENT_PREFIX_IOS,
+    android: USER_AGENT_PREFIX_ANDROID,
+});
 
 /**
  * Retrieves the unique device ID for the user.
@@ -22,4 +37,20 @@ export const getDeviceId = async (): Promise<string | null> => {
     } catch {
         return null;
     }
+};
+
+/**
+ * UserAgent 불러오기
+ * - 시스템 `UserAgent`에 앱 전용 `UserAgent`를 이어붙여 구성
+ * - `APP_USER_AGENT_PREFIX` 포함여부를 바탕으로 플랫폼 환경 판단 가능
+ */
+export const getUserAgent = async (): Promise<string> => {
+    const systemUserAgent = await DeviceInfo.getUserAgent();
+    const appName = Config.VIEW_APP_NAME ?? '';
+
+    const appVersion = DeviceInfo.getVersion();
+    const buildNumber = DeviceInfo.getBuildNumber();
+    const platform = Platform.OS === 'ios' ? 'iOS' : 'Android';
+
+    return `${systemUserAgent} (${APP_USER_AGENT_PREFIX}; ${appName}/${appVersion}; ${platform}; Build:${buildNumber})`;
 };
