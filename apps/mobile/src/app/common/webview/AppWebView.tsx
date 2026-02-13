dbaa2cc3f8a0ca56c95966dae62d9a7c38a4ce32
raine-lemon <raine@lemoncloud.io>
2026-02-13 14:34:32 +0900

    feat: enhance AppWebView - 웹 스크립트 주입을 통한 디바이스 정보 추가 - userAgent주입 연동

--- apps/mobile/src/app/common/webview/AppWebView.tsx
+++ apps/mobile/src/app/common/webview/AppWebView.tsx
@@ -1,9 +1,60 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useEffect, useState } from 'react';
+import { ActivityIndicator, Platform, StyleSheet, View } from 'react-native';
 import { WebView, type WebViewProps } from 'react-native-webview';
+import DeviceInfo from 'react-native-device-info';
+import Config from 'react-native-config';
+
+import { getAppLanguage, getUserAgent } from '../utils';
 
 interface AppWebViewProps extends WebViewProps {}
 
 export const AppWebView = forwardRef<WebView, AppWebViewProps>((props, ref) => {
+    const [initData, setInitData] = useState<{ userAgent: string; script: string } | null>(null);
+
+    useEffect(() => {
+        /**
+         * 웹 스크립트 주입을 통해 디바이스 정보 저장
+         */
+        const prepareWebView = async () => {
+            const [userAgent, uniqueId] = await Promise.all([getUserAgent(), DeviceInfo.getUniqueId()]);
+
+            const appVersion = DeviceInfo.getVersion();
+            const buildNumber = DeviceInfo.getBuildNumber();
+            const deviceModel = DeviceInfo.getDeviceId();
+            const applicationName = DeviceInfo.getApplicationName();
+            const appLanguage = getAppLanguage();
+            const platform = Platform.OS === 'ios' ? 'iOS' : 'Android';
+            const stage = Config.VITE_ENV || 'PROD';
+
+            const injectionScript = `
+                (function() {
+                    window.CHATIC_APP_PLATFORM = '${platform}';
+                    window.CHATIC_APP_APPLICATION = '${applicationName}';
+                    window.CHATIC_APP_STAGE = '${stage}';
+                    window.CHATIC_APP_DEVICE_ID = '${uniqueId || ''}';
+                    window.CHATIC_APP_DEVICE_MODEL = '${deviceModel || ''}';
+                    window.CHATIC_APP_CURRENT_VERSION = '${appVersion}';
+                    window.CHATIC_APP_BUILD_NUMBER = '${buildNumber}';
+                    window.CHATIC_APP_CURRENT_LANGUAGE = '${appLanguage}';
+                })();
+                true;
+            `;
+            setInitData({
+                userAgent: userAgent,
+                script: injectionScript,
+            });
+        };
+        void prepareWebView();
+    }, []);
+
+    if (!initData) {
+        return (
+            <View style={styles.loadingContainer}>
+                <ActivityIndicator size="large" color="#888" />
+            </View>
+        );
+    }
+
     return (
         <WebView
             ref={ref}
@@ -12,7 +63,18 @@ export const AppWebView = forwardRef<WebView, AppWebViewProps>((props, ref) => {
             javaScriptEnabled={true}
             domStorageEnabled={true}
             allowsBackForwardNavigationGestures={true}
+            userAgent={initData.userAgent}
+            injectedJavaScriptBeforeContentLoaded={initData.script}
             {...props}
         />
     );
+});
+
+const styles = StyleSheet.create({
+    loadingContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
 });
