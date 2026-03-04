099555961d7eabb8047e8b66fbf9c3beeda8ebd6
shy-lemon <shy@lemoncloud.io>
2026-03-04 13:22:32 +0900

    chore: commit remaining changes

--- apps/mobile/src/app/features/main/screens/MainScreen.tsx
+++ apps/mobile/src/app/features/main/screens/MainScreen.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useMemo, useRef, useState } from 'react';
+import Config from 'react-native-config';
 
 import { AppWebView, FullScreenLoader, Logger } from '../../../common';
 import {
@@ -16,8 +17,7 @@ import type { MainScreenProps } from '../navigation';
 import { useIsFocused } from '@react-navigation/native';
 import { KeyboardAvoidingView, Platform, View } from 'react-native';
 
-// const webviewUrl = Config.VITE_WEBVIEW_BASE_URL ?? 'http://localhost:5003';
-const webviewUrl = 'http://localhost:5003';
+const webviewUrl = Config.VITE_WEBVIEW_BASE_URL ?? 'http://localhost:5003';
 
 export const MainScreen = ({ navigation }: MainScreenProps) => {
     const webViewRef = useRef<WebView>(null);
