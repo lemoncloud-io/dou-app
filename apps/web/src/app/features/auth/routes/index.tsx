7a48242663eab0cb4e121532d72cfd149b0b0b69
shy-lemon <shy@lemoncloud.io>
2026-03-09 16:25:58 +0900

    refactor: replace LoginPage with auto login, move token login to TokenTestLoginPage

--- apps/web/src/app/features/auth/routes/index.tsx
+++ apps/web/src/app/features/auth/routes/index.tsx
@@ -1,11 +1,12 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 
-import { LoginPage, LogoutPage, OAuthResponsePage, TokenLoginPage } from '../pages';
+import { LoginPage, LogoutPage, OAuthResponsePage, TokenLoginPage, TokenTestLoginPage } from '../pages';
 
 export const AuthRoutes = () => {
     return (
         <Routes>
             <Route path="login" element={<LoginPage />} />
+            <Route path="token-test-login" element={<TokenTestLoginPage />} />
             <Route path="logout" element={<LogoutPage />} />
             <Route path="oauth-response" element={<OAuthResponsePage />} />
             <Route path="token/:token" element={<TokenLoginPage />} />
