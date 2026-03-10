7cbcf8d93ce2fb9cdc4c5a567d8f53b0b28a71dc
shy-lemon <shy@lemoncloud.io>
2026-03-10 20:08:44 +0900

    feat: add internal login flow

--- apps/web/src/app/features/mypage/routes/index.tsx
+++ apps/web/src/app/features/mypage/routes/index.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 
-import { MyPage, ProfileEditPage } from '../pages';
+import { LoginFormPage, MyPage, ProfileEditPage } from '../pages';
 
 export const MyPageRoutes = () => {
     return (
         <Routes>
             <Route index element={<MyPage />} />
             <Route path="edit" element={<ProfileEditPage />} />
+            <Route path="login" element={<LoginFormPage />} />
         </Routes>
     );
 };
