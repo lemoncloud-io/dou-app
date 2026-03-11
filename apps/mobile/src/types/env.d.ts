26e67863b20a05d854d3c07135ec9c2d9c9a6afa
louis <louis@lemoncloud.io>
2026-03-11 16:37:03 +0900

    feat: add env types and dependencies

---
+++ apps/mobile/src/types/env.d.ts
@@ -0,0 +1,8 @@
+declare module '@env' {
+    export const VITE_ENV: string;
+    export const VITE_WEBVIEW_BASE_URL: string;
+    export const VITE_WS_ENDPOINT: string;
+    export const VITE_SUBSCRIPTION_IAP_SKUS_IOS: string;
+    export const VITE_SUBSCRIPTION_IAP_SKUS_ANDROID: string;
+    export const VIEW_APP_NAME: string;
+}
