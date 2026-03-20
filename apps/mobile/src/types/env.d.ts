586e71ed2f54347090b8ab0dee51a325d8a46e0d
raine-lemon <raine@lemoncloud.io>
2026-03-20 15:20:56 +0900

    feat: add subscription plans and Google client ID env vars

--- apps/mobile/src/types/env.d.ts
+++ apps/mobile/src/types/env.d.ts
@@ -4,5 +4,7 @@ declare module '@env' {
     export const VITE_WS_ENDPOINT: string;
     export const VITE_SUBSCRIPTION_IAP_SKUS_IOS: string;
     export const VITE_SUBSCRIPTION_IAP_SKUS_ANDROID: string;
+    export const VITE_SUBSCRIPTION_IAP_PLANS_ANDROID: string;
     export const VIEW_APP_NAME: string;
+    export const VITE_GOOGLE_WEB_CLIENT_ID: string;
 }
