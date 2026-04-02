b72453007bb5cc011a98f6d04bf9e9b5c5c2560a
raine-lemon <raine@lemoncloud.io>
2026-04-02 19:54:26 +0900

    refactor: remove unused repository

--- apps/web/src/app/shared/data/repository/index.ts
+++ apps/web/src/app/shared/data/repository/index.ts
@@ -1,7 +1,1 @@
-export * from './channelRepository';
 export * from './chatRepository';
-export * from './cloudRepository';
-export * from './joinRepository';
-export * from './siteRepository';
-export * from './userRepository';
-export * from './userTokenRepository';
