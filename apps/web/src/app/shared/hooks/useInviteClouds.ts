9ea28d378d8865c744dc601a3c397f4c9059ee86
raine-lemon <raine@lemoncloud.io>
2026-04-10 11:48:44 +0900

    refactor: use inviteRepository instead of InviteCloudRepository

--- apps/web/src/app/shared/hooks/useInviteClouds.ts
+++ apps/web/src/app/shared/hooks/useInviteClouds.ts
@@ -1,18 +1,21 @@
 import { useEffect, useState } from 'react';
 import { getMobileAppInfo } from '@chatic/app-messages';
 import type { InviteCloudView } from '@chatic/app-messages';
-import { inviteCloudRepository } from '../repository/repository';
+import { useInviteRepository } from '../data';
+import { useWebSocketV2Store } from '@chatic/socket';
 
 export const useInviteClouds = () => {
     const [inviteClouds, setInviteClouds] = useState<InviteCloudView[]>([]);
     const [isLoading, setIsLoading] = useState(false);
     const { isOnMobileApp } = getMobileAppInfo();
+    const cloudId = useWebSocketV2Store().cloudId;
+    const inviteRepository = useInviteRepository(cloudId);
 
     useEffect(() => {
         if (!isOnMobileApp) return;
         setIsLoading(true);
-        inviteCloudRepository()
-            .loadAll()
+        inviteRepository
+            .getInvites()
             .then(setInviteClouds)
             .catch(_e => {
                 // ignore
--- apps/web/src/app/shared/repository/repository/InviteCloudRepository.ts
+++
@@ -1,8 +0,0 @@
-import { createNativeDBAdapter } from '../../data/local/nativeDBAdapter';
-import type { CacheStorage } from '../../data/local/cacheStorage';
-import type { InviteCloudView } from '@chatic/app-messages';
-
-export type StorageRepository<T> = CacheStorage<T>;
-
-export const inviteCloudRepository = (): StorageRepository<InviteCloudView> =>
-    createNativeDBAdapter<InviteCloudView>('invitecloud', '');
